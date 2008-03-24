9e713ba5207eb702bbb9695f5bfe2d3239a0237d
hub <hub@368663e2-1418-0410-8098-f419c199fb93>
2008-03-24 00:37:18 +0000

    some stringutils, with test case
    git-svn-id: http://svn.gna.org/svn/niepce/trunk/niepce@150 368663e2-1418-0410-8098-f419c199fb93

---
+++ src/utils/stringutils.h
@@ -0,0 +1,51 @@
+/*
+ * niepce - utils/stringutils.h
+ *
+ * Copyright (C) 2008 Hubert Figuiere
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef __UTILS_STRINGUTILS_H__
+#define __UTILS_STRINGUTILS_H__
+
+#include <boost/range/value_type.hpp>
+
+namespace utils {
+
+/** join elements with a separator 
+ * boost has such an algorithm in later versions. This is not it, but does a
+ * similar thing.
+ */
+template<typename S, typename C>
+typename boost::range_value< S >::type join(const S & input, const C & separator)
+{
+	typename boost::range_value< S >::type joint;
+	typename S::const_iterator iter(input.begin());
+	bool first = true;
+	for( ; iter != input.end(); iter++) {
+		if(!first) {
+			joint += separator;
+		}
+		joint += *iter;
+		first = false;
+	}
+	return joint;
+}
+
+
+}
+
+#endif
---
+++ src/utils/teststringutils.cpp
@@ -0,0 +1,44 @@
+/*
+ * niepce - utils/teststringutils.cpp
+ *
+ * Copyright (C) 2008 Hubert Figuiere
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#include <boost/test/minimal.hpp>
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+#include "stringutils.h"
+
+int test_main( int, char *[] )             // note the name!
+{
+	std::vector<std::string> v;
+	v.push_back("a");
+	v.push_back("b");
+	v.push_back("c");
+
+	std::string s = utils::join(v, ", ");
+
+	BOOST_CHECK(s == "a, b, c");
+
+	return 0;
+}
+
