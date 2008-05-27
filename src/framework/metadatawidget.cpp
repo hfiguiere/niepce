5ee76de3745abc8a8a1749294dae20e26fc45079
hub <hub@368663e2-1418-0410-8098-f419c199fb93>
2008-05-27 05:42:16 +0000

    reindent
    git-svn-id: http://svn.gna.org/svn/niepce/trunk/niepce@186 368663e2-1418-0410-8098-f419c199fb93

--- src/framework/metadatawidget.cpp
+++ src/framework/metadatawidget.cpp
@@ -35,122 +35,122 @@
 
 namespace framework {
 
-	MetaDataWidget::MetaDataWidget(const Glib::ustring & title)
-		: Gtk::Expander(title),
-		  m_table(1, 2, false),
-		  m_fmt(NULL)
-	{
-		add(m_table);
-		set_expanded(true);
-	}
+MetaDataWidget::MetaDataWidget(const Glib::ustring & title)
+    : Gtk::Expander(title),
+      m_table(1, 2, false),
+      m_fmt(NULL)
+{
+    add(m_table);
+    set_expanded(true);
+}
 
-	void MetaDataWidget::set_data_format(const xmp::MetaDataSectionFormat * fmt)
-	{
-		m_fmt = fmt;
-	}
+void MetaDataWidget::set_data_format(const xmp::MetaDataSectionFormat * fmt)
+{
+    m_fmt = fmt;
+}
 
-	namespace {
-		static 
-		void clear_widget(std::pair<const std::string, Gtk::Widget *> & p)
-		{
-			Gtk::Label * l = dynamic_cast<Gtk::Label*>(p.second);
-			if(l) {
-				l->set_text("");
-			}
-		}
-	}
+namespace {
+static 
+void clear_widget(std::pair<const std::string, Gtk::Widget *> & p)
+{
+    Gtk::Label * l = dynamic_cast<Gtk::Label*>(p.second);
+    if(l) {
+        l->set_text("");
+    }
+}
+}
 
-	void MetaDataWidget::set_data_source(const utils::XmpMeta * xmp)
-	{
-		DBG_OUT("set data source");
-		if(!m_data_map.empty()) {
-			std::for_each(m_data_map.begin(), m_data_map.end(),
-						  boost::bind(&clear_widget, _1));
-		}
-		if(!xmp) {
-			return;
-		}
-		if(!m_fmt) {
-			DBG_OUT("empty format");
-			return;
-		}
+void MetaDataWidget::set_data_source(const utils::XmpMeta * xmp)
+{
+    DBG_OUT("set data source");
+    if(!m_data_map.empty()) {
+        std::for_each(m_data_map.begin(), m_data_map.end(),
+                      boost::bind(&clear_widget, _1));
+    }
+    if(!xmp) {
+        return;
+    }
+    if(!m_fmt) {
+        DBG_OUT("empty format");
+        return;
+    }
 
-		const xmp::MetaDataFormat * current = m_fmt->formats;
-		XmpStringPtr value = xmp_string_new();
-		while(current && current->label) {
-			std::string id(current->property);
-			id += "-";
-			id += current->ns;
-			if(current->type == xmp::META_DT_STRING_ARRAY) {
-				XmpIteratorPtr iter = xmp_iterator_new(xmp->xmp(), current->ns,
-												  current->property, XMP_ITER_JUSTLEAFNODES);
-				std::vector<std::string> vec;
-				while(xmp_iterator_next(iter, NULL, NULL, value, NULL)) {
-					vec.push_back(xmp_string_cstr(value));
-				}
-				std::string v = utils::join(vec, ", ");
-				add_data(id, current->label, v.c_str(), current->type);
-                xmp_iterator_free(iter);
-			}
-			else {
-				const char * v = "";
-				if(xmp_get_property(xmp->xmp(), current->ns,
-									current->property, value, NULL)) {
-					v = xmp_string_cstr(value);
-				}
-				else {
-					DBG_OUT("get_property failed id = %s, ns = %s, prop = %s,"
-							"label = %s",
-							id.c_str(), current->ns, current->property,
-							current->label);
-				}
-				add_data(id, current->label, v, current->type);
-			}
-			current++;
-		}
-		xmp_string_free(value);
-	}
+    const xmp::MetaDataFormat * current = m_fmt->formats;
+    XmpStringPtr value = xmp_string_new();
+    while(current && current->label) {
+        std::string id(current->property);
+        id += "-";
+        id += current->ns;
+        if(current->type == xmp::META_DT_STRING_ARRAY) {
+            XmpIteratorPtr iter = xmp_iterator_new(xmp->xmp(), current->ns,
+                                                   current->property, XMP_ITER_JUSTLEAFNODES);
+            std::vector<std::string> vec;
+            while(xmp_iterator_next(iter, NULL, NULL, value, NULL)) {
+                vec.push_back(xmp_string_cstr(value));
+            }
+            std::string v = utils::join(vec, ", ");
+            add_data(id, current->label, v.c_str(), current->type);
+            xmp_iterator_free(iter);
+        }
+        else {
+            const char * v = "";
+            if(xmp_get_property(xmp->xmp(), current->ns,
+                                current->property, value, NULL)) {
+                v = xmp_string_cstr(value);
+            }
+            else {
+                DBG_OUT("get_property failed id = %s, ns = %s, prop = %s,"
+                        "label = %s",
+                        id.c_str(), current->ns, current->property,
+                        current->label);
+            }
+            add_data(id, current->label, v, current->type);
+        }
+        current++;
+    }
+    xmp_string_free(value);
+}
 
 
-	void MetaDataWidget::add_data(const std::string & id, 
-								  const std::string & label,
-								  const char * value,
-								  xmp::MetaDataType /*type*/)
-	{
-		Gtk::Label *w = NULL;
-		int n_row;
-		std::map<std::string, Gtk::Widget *>::iterator iter 
-			= m_data_map.end();
-		if(m_data_map.empty()) {
-			n_row = 0;
-		}
-		else {
-			iter = m_data_map.find(id);
-			n_row = m_table.property_n_rows();
-		}
-		if(iter == m_data_map.end()) {
-			Gtk::Label *labelw = Gtk::manage(new Gtk::Label(
-												 Glib::ustring("<b>") 
-												 + label + "</b>"));
-			labelw->set_alignment(0, 0.5);
-			labelw->set_use_markup(true);
+void MetaDataWidget::add_data(const std::string & id, 
+                              const std::string & label,
+                              const char * value,
+                              xmp::MetaDataType /*type*/)
+{
+    Gtk::Label *w = NULL;
+    int n_row;
+    std::map<std::string, Gtk::Widget *>::iterator iter 
+        = m_data_map.end();
+    if(m_data_map.empty()) {
+        n_row = 0;
+    }
+    else {
+        iter = m_data_map.find(id);
+        n_row = m_table.property_n_rows();
+    }
+    if(iter == m_data_map.end()) {
+        Gtk::Label *labelw = Gtk::manage(new Gtk::Label(
+                                             Glib::ustring("<b>") 
+                                             + label + "</b>"));
+        labelw->set_alignment(0, 0.5);
+        labelw->set_use_markup(true);
 
-			w = Gtk::manage(new Gtk::Label());
-			w->set_alignment(0, 0.5);
+        w = Gtk::manage(new Gtk::Label());
+        w->set_alignment(0, 0.5);
 
-			m_table.resize(n_row + 1, 2);
-			m_table.attach(*labelw, 0, 1, n_row, n_row+1, 
-						   Gtk::FILL, Gtk::SHRINK, 4, 0);
-			m_table.attach(*w, 1, 2, n_row, n_row+1, 
-						   Gtk::EXPAND|Gtk::FILL, Gtk::SHRINK, 4, 0);
-			m_data_map.insert(std::make_pair(id, w));
-		}
-		else {
-			w = static_cast<Gtk::Label*>(iter->second);
-		}
-		w->set_text(value);
-		m_table.show_all();
-	}
+        m_table.resize(n_row + 1, 2);
+        m_table.attach(*labelw, 0, 1, n_row, n_row+1, 
+                       Gtk::FILL, Gtk::SHRINK, 4, 0);
+        m_table.attach(*w, 1, 2, n_row, n_row+1, 
+                       Gtk::EXPAND|Gtk::FILL, Gtk::SHRINK, 4, 0);
+        m_data_map.insert(std::make_pair(id, w));
+    }
+    else {
+        w = static_cast<Gtk::Label*>(iter->second);
+    }
+    w->set_text(value);
+    m_table.show_all();
+}
 
 }
 
