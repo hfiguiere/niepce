75a56fbf3ce84a9c7ec1fcd499ac4716be759732
hub <hub@368663e2-1418-0410-8098-f419c199fb93>
2008-06-07 19:59:28 +0000

    reindent.
    git-svn-id: http://svn.gna.org/svn/niepce/trunk/niepce@189 368663e2-1418-0410-8098-f419c199fb93

--- src/utils/exempi.cpp
+++ src/utils/exempi.cpp
@@ -35,210 +35,210 @@ namespace bfs = boost::filesystem;
 
 namespace utils {
 
-	ExempiManager::ExempiManager(const ns_defs_t* namespaces)
-	{
-		if(xmp_init() && (namespaces != NULL)) {
-			const ns_defs_t* iter = namespaces;
-			for( ; iter->ns != NULL; iter++)	
-			{
-				// TODO check the return code
-				xmp_register_namespace(iter->ns,
-									   iter->prefix, NULL);
-			}
-		}
-	}
+ExempiManager::ExempiManager(const ns_defs_t* namespaces)
+{
+    if(xmp_init() && (namespaces != NULL)) {
+        const ns_defs_t* iter = namespaces;
+        for( ; iter->ns != NULL; iter++)	
+        {
+            // TODO check the return code
+            xmp_register_namespace(iter->ns,
+                                   iter->prefix, NULL);
+        }
+    }
+}
 
 
-	ExempiManager::~ExempiManager()
-	{
-		xmp_terminate();
-	}
+ExempiManager::~ExempiManager()
+{
+    xmp_terminate();
+}
 
 
-	XmpMeta::XmpMeta()
-		: m_xmp(),
-		  m_keyword_fetched(false)		  
-	{
-		m_xmp = xmp_new_empty();
-	}
+XmpMeta::XmpMeta()
+    : m_xmp(),
+      m_keyword_fetched(false)		  
+{
+    m_xmp = xmp_new_empty();
+}
 
 
-	/** @param file the path to the file to open 
-	 * @param sidecar_only we only want the sidecar.
-	 * It will locate the XMP sidecar for the file.
-	 */
-	XmpMeta::XmpMeta(const bfs::path & file, bool sidecar_only)
-		: m_xmp(NULL),
-		  m_keyword_fetched(false)
-	{
-		if(!sidecar_only) {
-			DBG_OUT("trying to load the XMP from the file");
-            xmp::ScopedPtr<XmpFilePtr> 
-                xmpfile(xmp_files_open_new(file.string().c_str(), XMP_OPEN_READ));
-			if(xmpfile != NULL) {
-				m_xmp = xmp_files_get_new_xmp(xmpfile);
-				if(xmpfile == NULL) {
-					ERR_OUT("xmpfile is NULL");
-				}
-			}
-		}
+/** @param file the path to the file to open 
+ * @param sidecar_only we only want the sidecar.
+ * It will locate the XMP sidecar for the file.
+ */
+XmpMeta::XmpMeta(const bfs::path & file, bool sidecar_only)
+    : m_xmp(NULL),
+      m_keyword_fetched(false)
+{
+    if(!sidecar_only) {
+        DBG_OUT("trying to load the XMP from the file");
+        xmp::ScopedPtr<XmpFilePtr> 
+            xmpfile(xmp_files_open_new(file.string().c_str(), XMP_OPEN_READ));
+        if(xmpfile != NULL) {
+            m_xmp = xmp_files_get_new_xmp(xmpfile);
+            if(xmpfile == NULL) {
+                ERR_OUT("xmpfile is NULL");
+            }
+        }
+    }
 		
-		if(m_xmp == NULL) {
-			size_t len;
-			char * buffer;
-			bfs::path sidecar = file.branch_path()
-				/ (basename(file) + ".xmp");
+    if(m_xmp == NULL) {
+        size_t len;
+        char * buffer;
+        bfs::path sidecar = file.branch_path()
+            / (basename(file) + ".xmp");
 			
-			DBG_OUT("creating xmpmeta from %s", sidecar.string().c_str());
-			FILE * f = fopen(sidecar.string().c_str(), "rb");
+        DBG_OUT("creating xmpmeta from %s", sidecar.string().c_str());
+        FILE * f = fopen(sidecar.string().c_str(), "rb");
 			
-			if (f != NULL) {
-				fseek(f, 0, SEEK_END);
-				len = ftell(f);
-				fseek(f, 0, SEEK_SET);
+        if (f != NULL) {
+            fseek(f, 0, SEEK_END);
+            len = ftell(f);
+            fseek(f, 0, SEEK_SET);
 				
-				buffer = (char*)malloc(len + 1);
-				/*size_t rlen =*/ fread(buffer, 1, len, f);
-				m_xmp = xmp_new_empty();
-				if(!xmp_parse(m_xmp, buffer, len)) {
-					xmp_free(m_xmp);
-					m_xmp = NULL;
-				}
-				free(buffer);
-			}
-		}
-	}
+            buffer = (char*)malloc(len + 1);
+            /*size_t rlen =*/ fread(buffer, 1, len, f);
+            m_xmp = xmp_new_empty();
+            if(!xmp_parse(m_xmp, buffer, len)) {
+                xmp_free(m_xmp);
+                m_xmp = NULL;
+            }
+            free(buffer);
+        }
+    }
+}
 
-	XmpMeta::~XmpMeta()
-	{
-		if(m_xmp) {
-			xmp_free(m_xmp);
-		}
-	}
+XmpMeta::~XmpMeta()
+{
+    if(m_xmp) {
+        xmp_free(m_xmp);
+    }
+}
 
-	std::string XmpMeta::serialize_inline() const
-	{
-		std::string buf;
-        xmp::ScopedPtr<XmpStringPtr> output(xmp_string_new());
-		if(xmp_serialize_and_format(m_xmp, output, 
-									XMP_SERIAL_OMITPACKETWRAPPER | XMP_SERIAL_OMITALLFORMATTING, 
-									0, "", "", 0)) {
-			buf = xmp_string_cstr(output);
-		}
-		return buf;
-	}
+std::string XmpMeta::serialize_inline() const
+{
+    std::string buf;
+    xmp::ScopedPtr<XmpStringPtr> output(xmp_string_new());
+    if(xmp_serialize_and_format(m_xmp, output, 
+                                XMP_SERIAL_OMITPACKETWRAPPER | XMP_SERIAL_OMITALLFORMATTING, 
+                                0, "", "", 0)) {
+        buf = xmp_string_cstr(output);
+    }
+    return buf;
+}
 
-	std::string XmpMeta::serialize() const
-	{
-		std::string buf;
-        xmp::ScopedPtr<XmpStringPtr> output(xmp_string_new());
-		if(xmp_serialize_and_format(m_xmp, output, 
-									XMP_SERIAL_OMITPACKETWRAPPER, 
-									0, "\n", " ", 0)) {
-			buf = xmp_string_cstr(output);
-		}
-		return buf;
-	}
+std::string XmpMeta::serialize() const
+{
+    std::string buf;
+    xmp::ScopedPtr<XmpStringPtr> output(xmp_string_new());
+    if(xmp_serialize_and_format(m_xmp, output, 
+                                XMP_SERIAL_OMITPACKETWRAPPER, 
+                                0, "\n", " ", 0)) {
+        buf = xmp_string_cstr(output);
+    }
+    return buf;
+}
 
-	void XmpMeta::unserialize(const char * buffer)
-	{
-		if(!buffer) 
-			return;
-		xmp_parse(m_xmp, buffer, strlen(buffer));
-	}
+void XmpMeta::unserialize(const char * buffer)
+{
+    if(!buffer) 
+        return;
+    xmp_parse(m_xmp, buffer, strlen(buffer));
+}
 
 
-	int32_t XmpMeta::orientation() const
-	{
-		int32_t _orientation = 0;
+int32_t XmpMeta::orientation() const
+{
+    int32_t _orientation = 0;
+    xmp::ScopedPtr<XmpStringPtr> value(xmp_string_new());
+    if(xmp_get_property(m_xmp, NS_TIFF, "Orientation", value, NULL)) {
+        try {
+            _orientation = boost::lexical_cast<int32_t>(xmp_string_cstr(value));
+        }
+        catch(const boost::bad_lexical_cast &)
+        {
+        }
+    }
+    return _orientation;
+}
+
+
+std::string XmpMeta::label() const
+{
+    std::string _label;
+    xmp::ScopedPtr<XmpStringPtr> value(xmp_string_new());
+    if(xmp_get_property(m_xmp, NS_XAP, "Label", value, NULL)) {
+        _label = xmp_string_cstr(value);
+    }
+    return _label;
+}
+
+
+int32_t XmpMeta::rating() const
+{
+    int32_t _rating = -1;
+    xmp::ScopedPtr<XmpStringPtr> value(xmp_string_new());
+    if(xmp_get_property(m_xmp, NS_XAP, "Rating", value, NULL)) {
+        try {
+            _rating = boost::lexical_cast<int32_t>(xmp_string_cstr(value));
+        }
+        catch(const boost::bad_lexical_cast &)
+        {
+        }
+    }
+    return _rating;
+}
+
+
+time_t  XmpMeta::creation_date() const
+{
+    time_t date = 0;
+    XmpDateTime value;
+    if(xmp_get_property_date(m_xmp, NS_EXIF, "DateTimeOriginal", 
+                             &value, NULL)) {
+        struct tm dt;
+        dt.tm_sec = value.second;
+        dt.tm_min = value.minute;
+        dt.tm_hour = value.hour;
+        dt.tm_mday = value.day;
+        dt.tm_mon = value.month;
+        dt.tm_year = value.year - 1900;
+        dt.tm_isdst = -1;
+        // this field is supposed to be a glibc extension. oh joy.
+        dt.tm_gmtoff = value.tzSign * ((value.tzHour * 3600) +
+                                       (value.tzMinute * 60));
+        date = mktime(&dt);
+        DBG_ASSERT(date != -1, "date is -1");
+    }
+    return date;
+}
+
+std::string XmpMeta::creation_date_str() const
+{
+    std::string s;
+    xmp::ScopedPtr<XmpStringPtr> value(xmp_string_new());
+    if(xmp_get_property(m_xmp, NS_EXIF, "DateTimeOriginal", 
+                        value, NULL)) {
+        s = xmp_string_cstr(value);
+    }
+    return s;
+}
+
+
+const std::vector< std::string > & XmpMeta::keywords() const
+{
+    if(!m_keyword_fetched) {
+        xmp::ScopedPtr<XmpIteratorPtr> iter(xmp_iterator_new(m_xmp, NS_DC, "subject", 
+                                                             XMP_ITER_JUSTLEAFNODES));
         xmp::ScopedPtr<XmpStringPtr> value(xmp_string_new());
-		if(xmp_get_property(m_xmp, NS_TIFF, "Orientation", value, NULL)) {
-			try {
-				_orientation = boost::lexical_cast<int32_t>(xmp_string_cstr(value));
-			}
-			catch(const boost::bad_lexical_cast &)
-			{
-			}
-		}
-		return _orientation;
-	}
-
-
-	std::string XmpMeta::label() const
-	{
-		std::string _label;
-        xmp::ScopedPtr<XmpStringPtr> value(xmp_string_new());
-		if(xmp_get_property(m_xmp, NS_XAP, "Label", value, NULL)) {
-			_label = xmp_string_cstr(value);
-		}
-		return _label;
-	}
-
-
-	int32_t XmpMeta::rating() const
-	{
-		int32_t _rating = -1;
-        xmp::ScopedPtr<XmpStringPtr> value(xmp_string_new());
-		if(xmp_get_property(m_xmp, NS_XAP, "Rating", value, NULL)) {
-			try {
-				_rating = boost::lexical_cast<int32_t>(xmp_string_cstr(value));
-			}
-			catch(const boost::bad_lexical_cast &)
-			{
-			}
-		}
-		return _rating;
-	}
-
-
-	time_t  XmpMeta::creation_date() const
-	{
-		time_t date = 0;
-		XmpDateTime value;
-		if(xmp_get_property_date(m_xmp, NS_EXIF, "DateTimeOriginal", 
-								 &value, NULL)) {
-			struct tm dt;
-			dt.tm_sec = value.second;
-			dt.tm_min = value.minute;
-			dt.tm_hour = value.hour;
-			dt.tm_mday = value.day;
-			dt.tm_mon = value.month;
-			dt.tm_year = value.year - 1900;
-			dt.tm_isdst = -1;
-			// this field is supposed to be a glibc extension. oh joy.
-			dt.tm_gmtoff = value.tzSign * ((value.tzHour * 3600) +
-										   (value.tzMinute * 60));
-			date = mktime(&dt);
-			DBG_ASSERT(date != -1, "date is -1");
-		}
-		return date;
-	}
-
-	std::string XmpMeta::creation_date_str() const
-	{
-		std::string s;
-        xmp::ScopedPtr<XmpStringPtr> value(xmp_string_new());
-		if(xmp_get_property(m_xmp, NS_EXIF, "DateTimeOriginal", 
-								 value, NULL)) {
-			s = xmp_string_cstr(value);
-		}
-		return s;
-	}
-
-
-	const std::vector< std::string > & XmpMeta::keywords() const
-	{
-		if(!m_keyword_fetched) {
-            xmp::ScopedPtr<XmpIteratorPtr> iter(xmp_iterator_new(m_xmp, NS_DC, "subject", 
-                                                                 XMP_ITER_JUSTLEAFNODES));
-            xmp::ScopedPtr<XmpStringPtr> value(xmp_string_new());
-			while(xmp_iterator_next(iter, NULL, NULL, value, NULL)) {
-				m_keywords.push_back(xmp_string_cstr(value));
-			}
-			m_keyword_fetched = true;
-		}
-		return m_keywords;
-	}
+        while(xmp_iterator_next(iter, NULL, NULL, value, NULL)) {
+            m_keywords.push_back(xmp_string_cstr(value));
+        }
+        m_keyword_fetched = true;
+    }
+    return m_keywords;
+}
 
 }
 
--- src/utils/exempi.h
+++ src/utils/exempi.h
@@ -78,27 +78,27 @@ private:
 };
 
 
-	enum MetaDataType {
-		META_DT_NONE = 0,
-		META_DT_STRING,
-		META_DT_STRING_ARRAY,
-		META_DT_DATE,
-		META_DT_STAR_RATING
-	};
+enum MetaDataType {
+    META_DT_NONE = 0,
+    META_DT_STRING,
+    META_DT_STRING_ARRAY,
+    META_DT_DATE,
+    META_DT_STAR_RATING
+};
 	
 	
-	struct MetaDataFormat {
-		const char * label;
-		const char * ns;
-		const char * property;
-		MetaDataType     type;
-		bool         readonly;
-	};
+struct MetaDataFormat {
+    const char * label;
+    const char * ns;
+    const char * property;
+    MetaDataType     type;
+    bool         readonly;
+};
 	
-	struct MetaDataSectionFormat {
-		const char * section;
-		const MetaDataFormat * formats;
-	};
+struct MetaDataSectionFormat {
+    const char * section;
+    const MetaDataFormat * formats;
+};
 	
 
 }
@@ -106,57 +106,57 @@ private:
 
 namespace utils {
 
-	class ExempiManager
-		: public boost::noncopyable
-	{
-	public:
-		struct ns_defs_t {
-			const char *ns;
-			const char *prefix;
-		};
-		/** construct with namespaces to initialize */
-		ExempiManager(const ns_defs_t * namespaces = 0);
-		~ExempiManager();
-	};
+class ExempiManager
+    : public boost::noncopyable
+{
+public:
+    struct ns_defs_t {
+        const char *ns;
+        const char *prefix;
+    };
+    /** construct with namespaces to initialize */
+    ExempiManager(const ns_defs_t * namespaces = 0);
+    ~ExempiManager();
+};
 
-	/** a high-level wrapper for xmp */
-	class XmpMeta
-	{
-	public:
-		XmpMeta();
- 		XmpMeta(const boost::filesystem::path & for_file, bool sidecar_only);
-		virtual ~XmpMeta();
+/** a high-level wrapper for xmp */
+class XmpMeta
+{
+public:
+    XmpMeta();
+    XmpMeta(const boost::filesystem::path & for_file, bool sidecar_only);
+    virtual ~XmpMeta();
 
-		bool isOk() const 
-			{ return m_xmp != NULL; }
-		XmpPtr xmp() const
-			{ return m_xmp; }
-		/** serialize the XMP inline */
-		std::string serialize_inline() const;
-		/** serialize the XMP (for the sidecar) */
-		std::string serialize() const;
-		/** load the XMP from the unserialized buffer 
-		 * (NUL terminated) 
-		 */
-		void unserialize(const char *);
+    bool isOk() const 
+        { return m_xmp != NULL; }
+    XmpPtr xmp() const
+        { return m_xmp; }
+    /** serialize the XMP inline */
+    std::string serialize_inline() const;
+    /** serialize the XMP (for the sidecar) */
+    std::string serialize() const;
+    /** load the XMP from the unserialized buffer 
+     * (NUL terminated) 
+     */
+    void unserialize(const char *);
 
-		int32_t orientation() const;
-		std::string label() const;
-		/** return the rating, -1 is not found (not set) */
-		int32_t rating() const;
-		time_t  creation_date() const;
-		std::string creation_date_str() const;
-		const std::vector< std::string > & keywords() const;
-	private:
-		XmpMeta( const XmpMeta & ); // copy constructor
+    int32_t orientation() const;
+    std::string label() const;
+    /** return the rating, -1 is not found (not set) */
+    int32_t rating() const;
+    time_t  creation_date() const;
+    std::string creation_date_str() const;
+    const std::vector< std::string > & keywords() const;
+private:
+    XmpMeta( const XmpMeta & ); // copy constructor
 
-		XmpMeta & operator=(const XmpMeta &); // assignment
+    XmpMeta & operator=(const XmpMeta &); // assignment
 
-		XmpPtr m_xmp;
-		// caches
-		mutable bool m_keyword_fetched;
-		mutable std::vector< std::string > m_keywords;
-	};
+    XmpPtr m_xmp;
+    // caches
+    mutable bool m_keyword_fetched;
+    mutable std::vector< std::string > m_keywords;
+};
 }
 
 /*
