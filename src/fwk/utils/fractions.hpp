71accecb73c7096f5906011f0b35286f36e41eae
Hubert Figuiere <hub@figuiere.net>
2009-03-01 22:27:55 -0500

    * Add fraction utilities.

---
+++ src/fwk/utils/fractions.hpp
@@ -0,0 +1,44 @@
+/*
+ * niepce - fwk/utils/fractions.hpp
+ *
+ * Copyright (C) 2008-2009 Hubert Figuiere
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#ifndef __FWK_FRACTIONS_HPP__
+#define __FWK_FRACTIONS_HPP__
+
+#include <string>
+
+namespace fwk {
+
+
+/** convert a fraction string to a decimal */
+double fraction_to_decimal(const std::string & value);
+
+}
+
+#endif
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0))
+  indent-tabs-mode:nil
+  fill-column:80
+  End:
+*/
